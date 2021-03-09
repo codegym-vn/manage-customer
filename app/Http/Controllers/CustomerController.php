3941d385a71200c3626c9ce503def48cff65a51f
Nguyễn Thiên <4708693-thien.nguyen2@users.noreply.gitlab.com>
2021-03-09 22:27:35 +0700

    filter and search customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,7 +11,7 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::paginate(5);
         $cities = City::all();
         return view('customers.list', compact('customers', 'cities'));
     }
@@ -68,7 +68,8 @@ class CustomerController extends Controller
         return redirect()->route('customers.index');
     }
 
-    public function filterByCity(Request $request){
+    public function filterByCity(Request $request)
+    {
         $idCity = $request->input('city_id');
 
         $cityFilter = City::findOrFail($idCity);
@@ -78,5 +79,16 @@ class CustomerController extends Controller
         $cities = City::all();
 
         return view('customers.list', compact('customers', 'cities', 'totalCustomerFilter', 'cityFilter'));
+    }
+
+    public function search(Request $request)
+    {
+        $keyword = $request->input('keyword');
+        if (!$keyword) {
+            return redirect()->route('customers.index');
+        }
+        $customers = Customer::where('name', 'LIKE', '%' . $keyword . '%')->paginate(5);
+        $cities = City::all();
+        return view('customers.list', compact('customers', 'cities'));
     }
 }
